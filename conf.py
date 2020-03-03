5bede1b3efcfe72575ab6838fb74a6b20806bfb2
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2020-03-03 13:34:47 +0530

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,10 @@
+SSID = 'XXXX' 
+AUTH_TOKEN = 'XXXX' 
+FROM_NUMBER = 'XXXX'
+TO_NUMBER = 'XXXX'
+API_KEY = 'XXXX'
+DEVICE_ID = 'XXXX'
+FRAME_SIZE = 10
+MUL_FACTOR = 3
+
+#Kindly tune the values of frame size and multiplication factor based on your choice to get optimal functioning.
